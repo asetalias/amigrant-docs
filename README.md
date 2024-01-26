ee9629b7574a513fa17aed1a3765f4ccde674fa6
Shivam Shandilya <70915754+Vyogami@users.noreply.github.com>
2024-01-26 17:09:11 +0530

    chore: initalize nextra project

---
+++ README.md
@@ -0,0 +1,34 @@
+# Amigrant Documentation
+
+This documentation contains the project plan, project details, contribution guidelines, and conventions used for the [Amigrant].
+
+## Local Development
+
+To get started with contribution to documentation locally, follow these steps:
+
+1. Clone the repository:
+
+   ```shell
+   git clone https:/github.com/asetalias/amigrant-docs.git
+   ```
+
+> [!Tip]
+> You can use `bun`, `yarn`, `pnpm` and `npm` for the following steps.
+
+1. Install the dependencies using:
+
+    ```shell
+    bun install
+    ```
+
+1. Start the development server:
+
+    ```shell
+    bun next dev
+    ```
+
+1. Open your browser and visit `http://localhost:3000` to view the website.
+
+## License
+
+This project is licensed under the [MIT License](./LICENSE).
---
+++ theme.config.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from "next/image";
+// import { Balsamiq_Sans } from "next/font/google";
+
+// const balsamiq = Balsamiq_Sans({
+//   weight: "400",
+//   subsets: ["latin"],
+// });
+
+export default {
+  logo: (
+    <>
+      {/* <Image
+        src="/images/amigrant.png"
+        alt="logo"
+        width={38}
+        height={38}
+        style={{ marginLeft: ".4em" }}
+      /> */}
+      <span
+        // className={`${balsamiq.className} logo`}
+        // style={{ marginLeft: ".4em", marginTop: fontSize: "1.5em"}}
+        style={{fontSize: "1.5em"}}
+      >
+        {" "}
+        Amigrant
+      </span>
+    </>
+  ),
+
+  project: {
+    link: "https://github.com/asetalias/amigrant-docs",
+  },
+
+  docsRepositoryBase: "https://github.com/asetalias/amigrant-docs/blob/main",
+
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+    toggleButton: true,
+  },
+
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - Amigrant",
+    };
+  },
+
+  nextThemes: {
+    defaultTheme: "dark",
+  },
+
+  chat: {
+    link: "https://discord.gg/kEv4QbWshr",
+  },
+
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Amigrant Documentation" />
+      <meta property="og:description" content="Amigrant Documentation" />
+      <link rel="icon" href="/images/favicon.ico" />
+    </>
+  ),
+
+  footer: {
+    text: (
+      <>
+        <span style={{ marginLeft: "1em", marginTop: ".2em" }}>
+          <a href="https://asetalias.in" target="_blank">
+            Made with ❤️ by ALiAS
+          </a>
+        </span>
+      </>
+    ),
+  },
+
+  search: {
+    placeholder: "Search",
+  },
+};
